767b42572624de39859ffb35748f6059e92fbc10
TurbatBay <learnwithturu@gmail.com>
2023-01-29 17:55:59 +0800

    completed Accordion project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  // function toggle(id) {
+
+  //   const filteredText = questions.filter(() => questions.id !== id);
+  //   return filteredText;
+  // }
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions And Answers About Login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion
+                key={question.id}
+                title={question.title}
+                id={question.id}
+                info={question.info}
+                // toggle={toggle}
+              />
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, id, info, key, toggle }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  //nee state n info giin toggle n whether you want to see it or not
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}{" "}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
