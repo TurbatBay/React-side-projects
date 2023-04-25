37a6391823608b92c8727f2edddaa310d00ef3ee
TurbatBay <learnwithturu@gmail.com>
2023-04-25 12:07:57 +0800

    Checking push

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -5,6 +5,7 @@ import { FaBars } from 'react-icons/fa'
 const Navbar = () => {
   return <h2>navbar component</h2>
   console.log("Checking")
+  console.log("Now is it working ? ")
 }
 
 export default Navbar
